06f42adc963e00a6b19b296a45f0b1149130f731
shalviraj <73280944+shalviraj@users.noreply.github.com>
2020-12-13 00:25:04 +0000

    Full One Page Website

--- resources/sketch.js
+++ resources/sketch.js
@@ -13,7 +13,7 @@ function windowResized () {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0,0);
-    canvas.style('z-index', '-10')
+    canvas.style('z-index', '-10');
 }
 
 function draw() {
